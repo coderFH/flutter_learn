d2df1007f409dacb3d44fe84072f04ba74d6ee35
王福豪 <wangfuhao@dongyin.net>
2020-03-22 11:34:22 +0800

    修改第一次点击闪烁问题

--- learn_flutter/lib/douban/pages/main/bottom_bar_item.dart
+++ learn_flutter/lib/douban/pages/main/bottom_bar_item.dart
@@ -4,7 +4,8 @@ class FHBottomBarItem extends BottomNavigationBarItem {
   FHBottomBarItem(String iconName , String title)
       : super(
       title : Text(title),
-      icon: Image.asset("assets/images/tabbar/$iconName.png", width: 32,),
+      //gaplessPlayback: true 解决是图片的情况下,第一次点击时闪烁的现象
+      icon: Image.asset("assets/images/tabbar/$iconName.png", width: 32,gaplessPlayback: true,),
       activeIcon: Image.asset("assets/images/tabbar/${iconName}_active.png", width: 32,),
     );
 
